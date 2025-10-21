cd35c9ebd65065d5c117bc829e54d97a54061202
RitikTiwari-pixle <ritiktiwari_3005@outlook.com>
2025-10-21 12:04:24 +0530

    hii

--- weatherProject/weatherProject/wsgi.py
+++ weatherProject/weatherProject/wsgi.py
@@ -1,16 +1,17 @@
-"""
-WSGI config for weatherProject project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
+# Path to your project directory (the one with manage.py)
+# ======== !! CHANGE THIS !! ========
+path = '/home/YOUR-USERNAME/your-repo-name'  
+
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+# Set the settings module
+# ======== !! CHANGE THIS !! ========
+os.environ['DJANGO_SETTINGS_MODULE'] = 'weatherProject.settings'
+
+# Import the Django WSGI application
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weatherProject.settings')
-
 application = get_wsgi_application()
